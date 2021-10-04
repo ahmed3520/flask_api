10a213f7a0b7b86abf35e520ec31d01d5f68af71
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2021-10-04 20:53:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import urllib.request
-import ssl
-
-import cv2
-import urllib.request
-
-import requests
-from PIL import Image
-
-from flask import Flask, request, abort
-
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
- return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000)
+    return "<p>Hello, World!</p>"
