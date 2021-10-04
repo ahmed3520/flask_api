4619ef98349b10d4938afd170ebe84435655a2bf
Ahmed Taha <57797571+ahmed3520@users.noreply.github.com>
2021-10-04 20:49:51 +0200

    update flask

--- main.py
+++ main.py
@@ -15,46 +15,12 @@ from PIL import Image
 from flask import Flask, request, abort
 
 
-def dhash(image, hashSize=8):
-	# resize the input image, adding a single column (width) so we
-	# can compute the horizontal gradient
-	resized = cv2.resize(image, (hashSize + 1, hashSize))
-
-	# compute the (relative) horizontal gradient between adjacent
-	# column pixels
-	diff = resized[:, 1:] > resized[:, :-1]
-
-	# convert the difference image to a hash
-	return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
-
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
-    return 'home'
-@app.route('/hashi', methods=['GET', 'POST'])
-def hashing():
-    imageName = request.args.get('image')
-    urlImage = 'https://dwjz5q0kg4677.cloudfront.net/'+imageName
-    request_response = requests.head(urlImage)
-    status_code = request_response.status_code
-    website_is_up = status_code == 200
-    if website_is_up == False:
-        abort(404)
-    print("websitete is ippp", website_is_up)
-    ssl._create_default_https_context = ssl._create_unverified_context
-    print(urlImage)
-    urllib.request.urlretrieve(
-        urlImage,
-        "gfg")
-    image = cv2.imread("gfg")
+def hello_world():
+ return "<p>Hello, World!</p>"
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    cv2.waitKey(0)
-
-    imageHash = dhash(image)
-
-    return str(imageHash)
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000)
